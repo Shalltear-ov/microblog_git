d654978654c93fb2a0e087cd6c9ef36b8189d845
Shalltear-ov <103678267+Shalltear-ov@users.noreply.github.com>
2023-04-10 22:42:26 +0300

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from sqlalchemy import MetaData
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, metadata=metadata)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models, errors
