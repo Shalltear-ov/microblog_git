58a54e24011cbe12facde80b42f75e13f267e9b6
Shalltear-ov <103678267+Shalltear-ov@users.noreply.github.com>
2023-04-12 10:08:07 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
-from app import db
+from app import db, app
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 from flask_login import UserMixin
 from app import login
-
+import jwt
+from time import time
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -58,6 +59,20 @@ class User(UserMixin, db.Model):
             followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').encode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            user_id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(user_id)
 
 
 @login.user_loader
