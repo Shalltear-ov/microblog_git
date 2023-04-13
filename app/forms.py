536a4c3a2297f52319bf2e416ceaff909d47e9b0
Shalltear-ov <Damir.SakhbievGG@gmail.com>
2023-04-13 13:51:16 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -25,14 +25,12 @@ class RegistrationForm(FlaskForm):
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-    @staticmethod
-    def validate_username(username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
 
-    @staticmethod
-    def validate_email(email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
