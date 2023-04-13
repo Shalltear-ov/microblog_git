122427614993beca33e5239e821b54596597ffaf
Shalltear-ov <Damir.SakhbievGG@gmail.com>
2023-04-13 13:50:49 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from sqlalchemy import MetaData
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -22,6 +23,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db, metadata=metadata)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу."
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
